a1a7f6b94efe276faca434f26c8e2e8a9d515e3d
gdhjh82 <gdhjh82@gmail.com>
2025-02-25 16:23:28 +0900

    [hjh] 로그인 세션 성공

---
+++ semi_project/src/main/java/com/gn/user/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.gn.user.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.gn.user.vo.User;
+
+public class UserDao {
+
+	public User loginUser(SqlSession session, User chkUser) {
+		User user = session.selectOne("userMapper.userLogin", chkUser);
+		
+		return user;
+	}
+}
