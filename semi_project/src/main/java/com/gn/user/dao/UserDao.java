ff8fe963049f483931e851038d96ff6463ab7aa8
gdhjh82 <gdhjh82@gmail.com>
2025-02-25 16:28:42 +0900

    [hjh] 수정

--- semi_project/src/main/java/com/gn/user/dao/UserDao.java
+++ semi_project/src/main/java/com/gn/user/dao/UserDao.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.session.SqlSession;
 import com.gn.user.vo.User;
 
 public class UserDao {
-
 	public User loginUser(SqlSession session, User chkUser) {
 		User user = session.selectOne("userMapper.userLogin", chkUser);
 		
